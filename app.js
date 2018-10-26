83367ee3ecc7624dda11be36338ff1102c147fee
Aleksei Iurchak <2iurchak@gmail.com>
2018-10-26 16:53:38 +0500

    blah

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 Array.prototype.rand = function () {
-    return this[Math.floor(Math.random() * this.length)]
+  return this[Math.floor(Math.random() * this.length)]
 }
 var palette = ['#d6a2ad', '#c3b59f', '#a0af84', '#668f80', '#4a6670', '#706677', '#565264'];
-var wrapper = document.querySelector(".wrapper");
+var viewportHeight = document.documentElement.clientHeight;
+var viewportWidth = document.documentElement.clientWidth;
+var wrapper = document.querySelector('.wrapper');
 
 const createTile = (x, y, w, h) => {
   const tile = document.createElement('div');
@@ -21,17 +23,11 @@ const appendTile = (tile) => {
   const bottomLine = parseInt(tile.dataset.y) + parseInt(tile.dataset.height);
   tile.style.gridColumn = leftLine + '/' + rightLine;
   tile.style.gridRow = topLine + '/' + bottomLine;
-
-  console.log('x: ' + leftLine + '-' + rightLine + ",\n y: " + topLine + '-' + bottomLine);
   wrapper.appendChild(tile);
 }
 
-for (var i = 0; i < 256; i++) {
-  let x = i % 16 + 1;
-  let y = Math.floor(i / 16) + 1;
-  let tile = createTile(x, y, 1, 1);
-  appendTile(tile);
-}
+let tile = createTile(1, 1, 1, 1);
+appendTile(tile);
 
 
 
