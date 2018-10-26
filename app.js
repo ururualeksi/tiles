0c38134e24ca8fec108f9ce822c6e43ad37de9d0
Aleksei Iurchak <2iurchak@gmail.com>
2018-10-26 18:19:49 +0500

    refactor createTile() and appendTile()

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ Array.prototype.rand = function () {
   return this[Math.floor(Math.random() * this.length)]
 }
 const randomColor = () => "hsl(" + 360 * Math.random() + ',' +
-                 (50 + 70 * Math.random()) + '%,' +
-                 (85 + 10 * Math.random()) + '%)';
+(50 + 70 * Math.random()) + '%,' +
+(85 + 10 * Math.random()) + '%)';
 
 var wrapper = document.querySelector('.wrapper');
 
@@ -15,21 +15,17 @@ const getRowHeight = () => {
   return Math.round(parseInt(wrapperStyle.width) / 16 - parseInt(wrapperStyle.gridGap));
 }
 
-const createTile = (x, y, w, h) => {
+const createTile = (w, h) => {
   const tile = document.createElement('div');
   tile.style.background = randomColor()
-  tile.dataset.x = x;
-  tile.dataset.y = y;
   tile.dataset.width = w;
   tile.dataset.height = h;
   return tile;
 }
 
-const appendTile = (tile) => {
-  const leftLine = tile.dataset.x;
-  const rightLine = parseInt(tile.dataset.x) + parseInt(tile.dataset.width);
-  const topLine = tile.dataset.y;
-  const bottomLine = parseInt(tile.dataset.y) + parseInt(tile.dataset.height);
+const appendTile = (tile, leftLine, topLine) => {
+  const rightLine = leftLine + parseInt(tile.dataset.width);
+  const bottomLine = topLine + parseInt(tile.dataset.height);
   tile.style.gridColumn = leftLine + '/' + rightLine;
   tile.style.gridRow = topLine + '/' + bottomLine;
   wrapper.appendChild(tile);
@@ -37,6 +33,17 @@ const appendTile = (tile) => {
 
 wrapper.style.gridAutoRows = getRowHeight() + 'px';
 
-appendTile(createTile(1,1,1,1))
-appendTile(createTile(2,1,1,1))
+appendTile(createTile(16, 16), 1, 1)
+
+// appendTile(createTile(2,1,1,1))
 // appendTile(createTile(3,1,2,2))
+
+wrapper.addEventListener('click', (e) => {
+  const tile = e.target
+  console.log(tile.dataset);
+  const x1 = parseInt(tile.dataset.x);
+  const y1 = parseInt(tile.dataset.y);
+
+  appendTile(createTile(1,1,16,16))
+  tile.parentNode.removeChild(tile);
+})
